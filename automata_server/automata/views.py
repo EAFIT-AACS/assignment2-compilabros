9a4d9dacf6e0f3df3df5b7ce93c4b90182d95929
Hever Alfonso <haalfonsoj@eafit.edu.co>
2025-04-10 17:18:00 -0500

    Created web-related functions

--- automata_server/automata/views.py
+++ automata_server/automata/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .algorithms.ALGORITHM_1_LFCO_2025_HA_MV import run as run_algorithm1
+from .algorithms.ALGORITHM_2_LFCO_2025_HA_MV import run as run_algorithm2
+from .algorithms.ALGORITHM_3_LFCO_2025_HA_MV import run as run_algorithm3
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def algorithm1_view(request):
+    result = run_algorithm1()
+    return render(request, 'algorithm1.html', result)
+
+def algorithm2_view(request):
+    results = run_algorithm2()
+    return render(request, 'algorithm2.html', {'results': results})
+
+def algorithm3_view(request):
+    trees = run_algorithm3()
+    return render(request, 'algorithm3.html', {'trees': trees})
